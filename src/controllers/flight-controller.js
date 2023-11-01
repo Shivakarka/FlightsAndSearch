433f4e16c534251f19663dca2785d81e44138c4c
shivakarka <shivakarka@gmail.com>
2023-11-02 00:36:21 +0530

    Added flights search crud with few filters

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,27 +1,48 @@
-const {FlightService} = require('../services/index');
+const { FlightService } = require("../services/index");
 
 const flightService = new FlightService();
 
 const create = async (req, res) => {
-    try {
-        const flight = await flightService.createFlight(req.body);
-       return res.status(201).json({
-        data: flight,
-        success: true,
-        message: "Flight created successfully",
-        err: {},
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-        data: {},
-        success: false,
-        message: "Not able to create flight",
-        err: error,
-        });
-    }
-    }
+  try {
+    const flight = await flightService.createFlight(req.body);
+    return res.status(201).json({
+      data: flight,
+      success: true,
+      message: "Flight created successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create flight",
+      err: error,
+    });
+  }
+};
+
+const getAll = async (req, res) => {
+  try {
+    const response = await flightService.getAllFlightData(req.query);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Flights fetched successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch flights",
+      err: error,
+    });
+  }
+};
 
 module.exports = {
-    create,
+  create,
+  getAll,
 };
--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,10 +1,66 @@
+const { Op } = require("sequelize");
 const { Flights } = require("../models");
 
 class FlightRepository {
+  #createFilter(data) {
+    let filter = {};
+    if (data.arrivalAirportId) {
+      filter.arrivalAirportId = data.arrivalAirportId;
+    }
+    if (data.departureAirportId) {
+      filter.departureAirportId = data.departureAirportId;
+    }
+    // the commented code added an extra price property to the filter object
+    // so used the priceFilter array instead
+    // if (data.minPrice && data.maxPrice) {
+    //   Object.assign(filter, {
+    //     price: { [Op.between]: [data.minPrice, data.maxPrice] },
+    //   });
+    // }
+    let priceFilter = [];
+    if (data.minPrice) {
+      //   Object.assign(filter, { price: { [Op.gte]: data.minPrice } });
+      priceFilter.push({ price: { [Op.gte]: data.minPrice } });
+    }
+    if (data.maxPrice) {
+      //   Object.assign(filter, { price: { [Op.lte]: data.maxPrice } });
+      priceFilter.push({ price: { [Op.lte]: data.maxPrice } });
+    }
+    if (priceFilter.length > 0) {
+      Object.assign(filter, {
+        [Op.and]: priceFilter,
+      });
+    }
+    return filter;
+  }
+
   async createFlight(data) {
     try {
       const flight = await Flights.create(data);
       return flight;
+    } catch (error) {
+      console.log("Something went wrong in repository");
+      throw { error };
+    }
+  }
+
+  async getFlight(flightId) {
+    try {
+      const flight = await Flights.findByPk(flightId);
+      return flight;
+    } catch (error) {
+      console.log("Something went wrong in repository");
+      throw { error };
+    }
+  }
+
+  async getAllFlights(filter) {
+    try {
+      const filterObject = this.#createFilter(filter);
+      const flights = await Flights.findAll({
+        where: filterObject,
+      });
+      return flights;
     } catch (error) {
       console.log("Something went wrong in repository");
       throw { error };
